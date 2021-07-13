fed887628d6a1c9407e33ed1ba96992a6770816a
Zhypar <Zhypar28@gmail.com>
2021-07-13 21:17:33 +0600

    changed the ENGINE

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -90,7 +90,7 @@ WSGI_APPLICATION = 'mysite.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('DB_NAME'),
         'USER': config('DB_USER'),
         'PASSWORD': config('DB_PASSWORD'),
